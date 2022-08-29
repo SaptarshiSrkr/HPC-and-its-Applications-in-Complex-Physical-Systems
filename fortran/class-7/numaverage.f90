8b4717a6c95e5f5bf013ded5d19b3ff475b306b9
Saptarshi Sarkar <saptarshi5683@gmail.com>
2022-08-29 14:54:08 +0530

    corrected code

--- fortran/class-7/numaverage.f90
+++ fortran/class-7/numaverage.f90
@@ -2,35 +2,41 @@ program numaverage
  implicit none
  include 'mpif.h'
 
- integer :: ierr,N,j,i,nproc,rank,sum1
+ integer :: ierr,N,j,i,nproc,rank,total
  integer,allocatable :: sums(:)
  real :: average
  
  call mpi_init(ierr)
  call mpi_comm_size(mpi_comm_world,nproc,ierr)
  call mpi_comm_rank(mpi_comm_world,rank,ierr)
-
- N=100
- sum1=0
+ 
+ if(rank==0) then
+  write(*,*) 'Enter the number of integers: '
+  read(*,"(i5)") N
+ endif
+ 
+ call mpi_bcast(N,1,mpi_int,0,mpi_comm_world,ierr)
+ 
+ total=0
  allocate(sums(0:nproc-1))
 
  do i=1+rank,N,nproc
-  sum1=sum1+i
+  total=total+i
  enddo
  
  if(rank==0) then
-  sums(0)=sum1
+  sums(0)=total
  else
-  call mpi_send(sum1,1,mpi_int,0,mpi_any_tag,mpi_comm_world,ierr)
+  call mpi_send(total,1,mpi_int,0,rank,mpi_comm_world,ierr)
  endif
 
  if(rank==0) then
-  do j=1,nproc
-   call mpi_recv(sums(j),1,mpi_int,j,mpi_any_tag,mpi_comm_world,mpi_status_ignore,ierr)
+ 
+  do j=1,nproc-1
+   call mpi_recv(sums(j),1,mpi_int,j,j,mpi_comm_world,mpi_status_ignore,ierr)
   enddo
- average = sum(sums)/nproc
-
- write(*,*) 'The average of first ',N,' integers is: ',average
+  
+ write(*,'(a,i5,a,i10)') 'The sum of first ',N,' integers is: ',sum(sums)
  endif
  
  call mpi_finalize(ierr)
