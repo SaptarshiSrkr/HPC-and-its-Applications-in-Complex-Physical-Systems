daeaad38433aeaa4de8595dd8c087e857f813750
Saptarshi Sarkar <saptarshi5683@gmail.com>
2022-08-29 14:20:43 +0530

    added class 7

--- fortran/class-6/readwrite.f90
+++ fortran/class-6/readwrite.f90
@@ -2,12 +2,15 @@ program readwriteint
  implicit none
  include 'mpif.h'
  
- integer :: ierr, nproc, rank, i, N
+ integer :: ierr, nproc, rank, i, N,s
  real :: randints(1000), sumr, maxr, minr, sum_final, max_final, min_final
  
  call mpi_init(ierr)
  call mpi_comm_size(mpi_comm_world,nproc,ierr)
  call mpi_comm_rank(mpi_comm_world,rank,ierr)
+ 
+ s=131489+rank
+ call srand(s)
 
  N=1000
  
