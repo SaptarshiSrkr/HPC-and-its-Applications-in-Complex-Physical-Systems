11c6dcb7f52d985e917d1f09fc44b3672a8cd8c0
Saptarshi Sarkar <saptarshi5683@gmail.com>
2022-09-01 18:56:59 +0530

    added class 9

---
+++ fortran/class-9/mybarrier.f90
@@ -0,0 +1,43 @@
+program mybarrier
+ implicit none
+ include 'mpif.h'
+ 
+ integer :: ierr, nprocs, id, i, test
+ 
+ call mpi_init(ierr)
+ call mpi_comm_size(mpi_comm_world,nprocs,ierr)
+ call mpi_comm_rank(mpi_comm_world,id,ierr)
+
+ test = 1
+
+!Barrier Starts
+
+ if(id/=0) then
+  call mpi_send(test,1,mpi_integer,0,id,mpi_comm_world,ierr)
+  write(*,*) 'Process ',id,' at start of barrier'
+ endif
+
+ if(id==0) then
+  write(*,*) 'Process ',0,' at start of barrier'
+  do i=1,nprocs-1
+   call mpi_recv(test,1,mpi_integer,i,i,mpi_comm_world,mpi_status_ignore,ierr)
+   call mpi_send(test,1,mpi_integer,i,i,mpi_comm_world,ierr)
+  enddo
+  write(*,*) 'Process ',0,' at end of barrier'
+ end if
+
+ if(id/=0) then
+  call mpi_recv(test,1,mpi_integer,0,id,mpi_comm_world,mpi_status_ignore,ierr)
+  write(*,*) 'Process ',id,' at end of barrier'
+ endif
+
+!Barrier Ends
+
+ call mpi_finalize(ierr)
+end program mybarrier
+ 
+
+ 
+
+  
+ 
